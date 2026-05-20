58f886652dc9cfaa0ce045eb9973e9e343eb2a3a
Kreucyu <elciocleiton72@gmail.com>
2026-05-20 14:31:04 -0300

    chore: Instanciando services

--- src/main/java/com/produto_service/service/ProdutoService.java
+++ src/main/java/com/produto_service/service/ProdutoService.java
@@ -1,4 +1,12 @@
 package com.produto_service.service;
 
+import com.produto_service.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProdutoService {
+
+    @Autowired
+    ProdutoRepository produtoRepository;
 }

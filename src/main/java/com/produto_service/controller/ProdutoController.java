5cb43e3b1ff878a3ddc3c386a9da3318ba112ef3
Kreucyu <elciocleiton72@gmail.com>
2026-05-20 14:31:39 -0300

    chore: instanciando controller

--- src/main/java/com/produto_service/controller/ProdutoController.java
+++ src/main/java/com/produto_service/controller/ProdutoController.java
@@ -1,4 +1,11 @@
 package com.produto_service.controller;
 
+import com.produto_service.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProdutoController {
+    @Autowired
+    ProdutoService produtoService;
 }

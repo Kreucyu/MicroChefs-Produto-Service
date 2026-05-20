48a4b0456557fa2be40a96fbf1d584a4cfe05506
Kreucyu <elciocleiton72@gmail.com>
2026-05-20 14:30:29 -0300

    chore: instanciando repositorio

--- src/main/java/com/produto_service/repository/ProdutoRepository.java
+++ src/main/java/com/produto_service/repository/ProdutoRepository.java
@@ -1,4 +1,9 @@
 package com.produto_service.repository;
 
-public class ProdutoRepository {
+import com.produto_service.entities.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 }

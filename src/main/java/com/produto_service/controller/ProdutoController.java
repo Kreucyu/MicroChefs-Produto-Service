19465704c4d0ba9a3ba83bf746e07b03be73de2a
Kreucyu <elciocleiton72@gmail.com>
2026-05-24 11:14:05 -0300

    feat: endpoint POST para criar produto

--- src/main/java/com/produto_service/controller/ProdutoController.java
+++ src/main/java/com/produto_service/controller/ProdutoController.java
@@ -1,19 +1,26 @@
 package com.produto_service.controller;
 
+import com.produto_service.dto.CreateProdutoDTO;
 import com.produto_service.dto.RecoveryProdutoDTO;
 import com.produto_service.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@RestController("/produtos")
 public class ProdutoController {
     @Autowired
     ProdutoService produtoService;
-    @GetMapping
+
+    @PostMapping("/criar")
+    public ResponseEntity<CreateProdutoDTO> criarProduto(@RequestBody CreateProdutoDTO novoProduto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(produtoService.criarProduto(novoProduto));
+    }
+
+    @GetMapping("/listar")
     public ResponseEntity<List<RecoveryProdutoDTO>> listarProdutos() {
         return ResponseEntity.ok().body(produtoService.listarProdutos());
     }

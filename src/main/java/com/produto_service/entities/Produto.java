6b1259e0e22a7e78779311c3d0beb6959b5eb35b
Kreucyu <elciocleiton72@gmail.com>
2026-05-23 22:48:06 -0300

    build: implementando entidade produto

--- src/main/java/com/produto_service/entities/Produto.java
+++ src/main/java/com/produto_service/entities/Produto.java
@@ -1,5 +1,24 @@
 package com.produto_service.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
 
+@Table(name = "produtos")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(name = "nome")
+    private String nomeProduto;
+    @Column(name = "descricao")
+    private String descricaoProduto;
+    @Column(name = "quantidade")
+    private int quantidadeProduto;
+    @Column(name = "preco")
+    private double precoProduto;
 }

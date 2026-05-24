6cdce62243a7eb4bf0a4775a7dcf720651d6e05f
Kreucyu <elciocleiton72@gmail.com>
2026-05-23 22:44:05 -0300

    feat: criando método de listagem de produtos

--- src/main/java/com/produto_service/entities/Produto.java
+++ src/main/java/com/produto_service/entities/Produto.java
@@ -1,4 +1,5 @@
 package com.produto_service.entities;
 
+
 public class Produto {
 }
--- src/main/java/com/produto_service/service/ProdutoService.java
+++ src/main/java/com/produto_service/service/ProdutoService.java
@@ -1,8 +1,12 @@
 package com.produto_service.service;
 
+import com.produto_service.dto.RecoveryProdutoDTO;
+import com.produto_service.entities.Produto;
 import com.produto_service.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProdutoService {
@@ -10,5 +14,8 @@ public class ProdutoService {
     @Autowired
     ProdutoRepository produtoRepository;
 
-
+    public List<RecoveryProdutoDTO> listarProdutos() {
+        List<Produto> produtos = produtoRepository.findAll();
+        return produtos.stream().forEach(p -> new RecoveryProdutoDTO());
+    }
 }

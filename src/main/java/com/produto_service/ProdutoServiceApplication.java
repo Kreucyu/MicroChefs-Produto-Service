63aa136ce95339bdb3bd263975b72b602c9afc2c
Kreucyu <elciocleiton72@gmail.com>
2026-06-04 15:11:45 -0300

    build: declarando bd e porta para poder utilizar a conexao jdbc

--- src/main/java/com/produto_service/ProdutoServiceApplication.java
+++ src/main/java/com/produto_service/ProdutoServiceApplication.java
@@ -1,8 +1,10 @@
 package com.produto_service;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableRabbit
 @SpringBootApplication
 public class ProdutoServiceApplication {
 
